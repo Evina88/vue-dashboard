c33c33dc96a4faddb70d7ca59b989ed1e33493c0
Evina Mouselimi <evina@cgworks.com>
2022-07-22 13:00:17 +0300

    deployment

---
+++ deploy.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+
+
+set -e
+
+npm run build
+
+cd dist
+
+git init 
+
+git add -A
+
+git commit -m "new deployment"
+
+git push -f https://github.com/Evina88/vue-dashboard.git main:gh-branches
+
