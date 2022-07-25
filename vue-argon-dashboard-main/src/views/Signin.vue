d841450400c1d22fac0f208432c954b211358e86
Evina Mouselimi <evina@cgworks.com>
2022-07-25 14:27:55 +0300

    testing

--- vue-argon-dashboard-main/src/views/Signin.vue
+++ vue-argon-dashboard-main/src/views/Signin.vue
@@ -97,10 +97,10 @@
 </template>
 
 <script>
-	import Navbar from "./examples/PageLayout/Navbar.vue";
-	import ArgonInput from "./components/ArgonInput.vue";
-	import ArgonSwitch from "./components/ArgonSwitch.vue";
-	import ArgonButton from "./components/ArgonButton.vue";
+	import Navbar from "../examples/PageLayout/Navbar.vue";
+	import ArgonInput from "../components/ArgonInput.vue";
+	import ArgonSwitch from "../components/ArgonSwitch.vue";
+	import ArgonButton from "../components/ArgonButton.vue";
 	const body = document.getElementsByTagName("body")[0];
 
 	export default {
